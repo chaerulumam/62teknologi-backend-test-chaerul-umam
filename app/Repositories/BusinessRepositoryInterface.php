5f58d00c0972b08fc5a2eaeba5585ef277d1a242
Chaerul Umam <chaerulumamb244@gmail.com>
2023-10-30 00:17:39 +0700

    feat: Create deleted data.

--- app/Repositories/BusinessRepositoryInterface.php
+++ app/Repositories/BusinessRepositoryInterface.php
@@ -7,4 +7,5 @@ interface BusinessRepositoryInterface
     public function businessDetails($id);
     public function create(array $data);
     public function updateDataById(Int $id, array $data);
+    public function deleteDataById(Int $id);
 }
